3261f336bcb17694bf72cd8a6f39fcc538a0af90
lutfyis <lutfyikasutanti1@gmail.com>
2026-07-16 23:07:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,86 +1,42 @@
 // ===========================================
-// HR Attendance v1.0
-// Checkpoint 7 - Camera Engine
+// HR Attendance v1.0 - Stable
+// Checkpoint 7
 // ===========================================
 
 let html5QrCode;
 
-// ===========================================
 // Saat halaman dibuka
-// ===========================================
 window.onload = function () {
-    alert("script jalan");
-
     startScanner();
-
 };
 
-// ===========================================
-// Menyalakan Kamera
-// ===========================================
+// Menyalakan kamera
 function startScanner() {
 
     const status = document.getElementById("status");
-
     status.innerHTML = "🟡 Membuka kamera...";
 
     html5QrCode = new Html5Qrcode("reader");
 
-    Html5Qrcode.getCameras().then(cameras => {
-
-        if (cameras && cameras.length) {
-
-            let cameraId = cameras[cameras.length - 1].id;
-
-            html5QrCode.start(
-
-                cameraId,
-
-                {
-                    fps: 10,
-
-                    qrbox: {
-                        width: 250,
-                        height: 250
-                    }
-
-                },
-
-                onScanSuccess,
-
-                onScanFailure
-
-            ).then(() => {
-
-                status.innerHTML = "🟢 Ready to Scan";
-
-            });
-
-        } else {
-
-            status.innerHTML = "🔴 Kamera tidak ditemukan";
-
-        }
-
-    }).catch(err => {
-
-        alert(err);
-
-        console.error(err);
-
-        status.innerHTML = "🔴 Gagal membuka kamera";
-
+    Html5Qrcode.start(
+        "reader",
+        {
+            facingMode: "environment"
+        },
+        {
+            fps: 10,
+            qrbox: 250
+        },
+        onScanSuccess,
+        onScanFailure
+    ).catch(err => {
+        console.log(err);
+        status.innerHTML = "🔴 Kamera gagal dibuka";
     });
-
 }
 
-// ===========================================
-// QR Berhasil Dibaca
-// ===========================================
-function onScanSuccess(decodedText) {
-
-    // Hentikan scanner supaya QR tidak terbaca berkali-kali
-    html5QrCode.stop();
+// QR berhasil terbaca
+function onScanSuccess(decodedText, decodedResult) {
 
     document.getElementById("status").innerHTML = `
         <h3>✅ QR Terdeteksi</h3>
@@ -88,16 +44,11 @@ function onScanSuccess(decodedText) {
         <b>${decodedText}</b>
     `;
 
-    console.log("QR :", decodedText);
+    console.log(decodedText);
 
 }
 
-// ===========================================
-// QR Gagal Dibaca
-// ===========================================
+// QR belum terbaca
 function onScanFailure(error) {
-
-    // Sengaja dikosongkan
-    // Agar tidak memenuhi console
-
+    // dikosongkan
 }
